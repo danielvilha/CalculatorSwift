71a659a6e5221f72688e26ef15c2a5eb759dee81
Daniel Ferreira de Lima Freitas Vilha <danielvilha@gmail.com>
2020-01-11 00:25:07 +0000

    I created a new struct called CalculatorButtonView with the calculator button calls, and also removed the Landscape orientation calls.

--- CalculatorSwift/ContentView.swift
+++ CalculatorSwift/ContentView.swift
@@ -9,8 +9,9 @@
 import SwiftUI
 
 enum CalculatorButton: String {
-    case zero, one, two, three, four, five, six, sevent, eigth, nine, decimal
+    case zero, one, two, three, four, five, six, sevent, eigth, nine
     case equals, plus, minus, multiply, divide
+    case decimal
     case ac, plusMinus, percent
     
     var title: String {
@@ -52,7 +53,18 @@ enum CalculatorButton: String {
     }
 }
 
+// Environment Object
+class GlobalEnvironment: ObservableObject {
+    @Published var display = ""
+    
+    func receiveInput(calculatorButton: CalculatorButton) {
+        self.display = calculatorButton.title
+    }
+}
+
 struct ContentView: View {
+    
+    @EnvironmentObject var env: GlobalEnvironment
     
     let buttons: [[CalculatorButton]] = [
         [.ac, .plusMinus, .percent, .divide],
@@ -69,32 +81,42 @@ struct ContentView: View {
             VStack(spacing: 12) {
                 HStack {
                     Spacer()
-                    Text("42").foregroundColor(Color.white)
+                    Text(env.display).foregroundColor(Color.white)
                         .font(.system(size: 64))
                 }.padding()
                 
                 ForEach(buttons, id: \.self) { row in
                     HStack(spacing: 12) {
                         ForEach(row, id: \.self) { button in
-                            Button(action: {
-                                
-                            }) {
-                                Text(button.title)
-                                    .font(.system(size: 32))
-                                    .frame(width: self.buttonWidth(button: button),
-                                           height: (UIScreen.main.bounds.width - 5 * 12) / 4)
-                                    .foregroundColor(Color.white)
-                                    .background(button.backgroundColor)
-                                    .cornerRadius(self.buttonWidth(button: button))
-                                }
+                            CalculatorButtonView(button: button)
                         }
                     }
                 }
             }
         }.padding(.bottom)
     }
+}
+
+struct CalculatorButtonView: View {
+    var button: CalculatorButton
     
-    func buttonWidth(button: CalculatorButton) -> CGFloat {
+    @EnvironmentObject var env: GlobalEnvironment
+    
+    var body: some View {
+        Button(action: {
+            self.env.receiveInput(calculatorButton: self.button)
+        }) {
+            Text(button.title)
+                .font(.system(size: 32))
+                .frame(width: self.buttonWidth(button: button),
+                       height: (UIScreen.main.bounds.width - 5 * 12) / 4)
+                .foregroundColor(Color.white)
+                .background(button.backgroundColor)
+                .cornerRadius(self.buttonWidth(button: button))
+            }
+    }
+    
+    private func buttonWidth(button: CalculatorButton) -> CGFloat {
         if (button == .zero) {
             return (UIScreen.main.bounds.width - 4 * 12) / 4 * 2
         }
@@ -105,6 +127,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(GlobalEnvironment())
     }
 }
